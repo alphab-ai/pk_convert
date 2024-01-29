17eb7d164924240ccac81712252a6aa1526deaf6
Northa <Northa@users.noreply.github.com>
2024-01-29 22:50:18 +0000

    init commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"os"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	"github.com/cosmos/cosmos-sdk/crypto"
+)
+
+// REPLACE THIS SHIT WITH YOUR VALUE 
+var PRIVKEY = "REPLACE THIS STR WITH YOUR PRIVATE KEY"
+
+func main() {
+
+	privKeyBytes, err := hex.DecodeString(PRIVKEY)
+	
+	if err != nil {
+		fmt.Println("Error decoding hex private key:", err)
+		os.Exit(1)
+	}
+
+	privKey := &secp256k1.PrivKey{
+		Key: privKeyBytes,
+	}
+
+	armoredString := crypto.EncryptArmorPrivKey(privKey, "12345678", "secp256k1")
+
+	fmt.Println("Armored Private Key:\n")
+	fmt.Println(armoredString)
+}
